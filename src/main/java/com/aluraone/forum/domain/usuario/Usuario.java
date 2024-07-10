43013f1418f46324c1c7d21e5d9bc4f82bc4c467
Walttinho <walter.netto@live.com>
2024-07-10 00:04:28 -0300

    feat(domain): add Usuario entity     - Added Usuario entity with UserDetails implementation.

---
+++ src/main/java/com/aluraone/forum/domain/usuario/Usuario.java
@@ -0,0 +1,73 @@
+package com.aluraone.forum.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.aluraone.forum.domain.topico.Topico;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+@Table(name="usuarios")
+@Entity(name="Usuario")
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+
+public class Usuario implements UserDetails {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String nome;
+  private String email;
+  private String senha;
+  private boolean status;
+
+  @OneToMany(mappedBy = "autor")
+  private List<Topico> topicos = new ArrayList<>();
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+  return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+  }
+
+  @Override
+  public String getPassword() {
+    return senha;
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return UserDetails.super.isAccountNonExpired();
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return UserDetails.super.isAccountNonLocked();
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return UserDetails.super.isCredentialsNonExpired();
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return UserDetails.super.isEnabled();
+  }
+}
